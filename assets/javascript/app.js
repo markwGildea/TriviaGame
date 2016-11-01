560ac8a33bf932b09b08662aa790bbdc35405e30
Mark Gildea <gildeamark1@gmail.com>
2016-11-01 14:46:41 -0500

    getting there

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var number = 90;
+
+	function run(){
+		counter = setInterval(decrement, 1000);
+	}
+
+	function decrement(){
+		number--;
+		$('#show-number').html("<h3>Time Remaining: " + number + " seconds</h3>");
+		if (number === 0){
+			stop();
+			alert('Time is up!');
+		}
+	}
+
+	function stop(){
+		clearInterval(counter);
+	}
+
+	run();
+	
