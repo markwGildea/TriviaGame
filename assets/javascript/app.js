c3ee15c5f650d5bee460c14cf37f399f9ccedee3
Mark Gildea <gildeamark1@gmail.com>
2016-11-03 11:53:04 -0500

    getting there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,22 @@
 var number = 121;
 
-	function run(){
-		counter = setInterval(decrement, 1000);
-	}
+function run() {
+    counter = setInterval(decrement, 1000);
+}
 
-	function decrement(){
-		number--;
-		$('#show-number').html("<h3>Time Remaining: " + number + " seconds</h3>");
-		if (number === 0){
-			stop();
-			alert('Time is up!');
-		}
-	}
+function decrement() {
+    number--;
+    $('#show-number').html("<h3>Time Remaining: " + number + " seconds</h3>");
+    if (number === 0) {
+        stop();
+        alert('Time is up!');
+    }
+}
 
-	function stop(){
-		clearInterval(counter);
-	}
+function stop() {
+    clearInterval(counter);
+}
 
-	run();
+run();
 
-	$("#choice1").click(function () {
-      });
-
-	
+$("#choice1").click(function() {});
